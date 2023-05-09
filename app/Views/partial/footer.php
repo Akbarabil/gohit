3715bc2f1de0ad066201083256938b989a7d425d
Varis anggito <varisanggito87@gmail.com>
2023-05-09 09:47:49 +0700

    footer

--- app/Views/partial/footer.php
+++ app/Views/partial/footer.php
@@ -1,11 +1,11 @@
-<footer class="py-3 my-4 text-center " style="background-color: whitesmoke;">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <img src="<?= base_url('assets/image/logo.png') ?>" alt="" srcset="" class="">
-    <p class="text-center text-muted">© 2023 Gohit, Inc</p>
-  </footer>
+<footer class="footer-59391" style="background-color: whitesmoke;">
+  <ul class="nav justify-content-center border-bottom pb-3 mb-3 ">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+  </ul>
+  <center><img src="<?= base_url('assets/image/logo.png') ?>" alt="" srcset="" class=""></center>
+  <p class="text-center text-muted">© 2023 Gohit, Inc</p>
+</footer>
